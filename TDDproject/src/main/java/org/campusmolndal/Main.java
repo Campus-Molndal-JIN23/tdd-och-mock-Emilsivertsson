e468d6938d79bfb29fb28852bd0c178d906bb3e1
Emilsivertsson <Emil.sivertsson@gmail.com>
2023-06-05 13:07:09 +0200

    started tests

--- TDDproject/src/main/java/org/campusmolndal/Main.java
+++ TDDproject/src/main/java/org/campusmolndal/Main.java
@@ -1,5 +1,9 @@
 package org.campusmolndal;
 
+import org.campusmolndal.Utmaning.WeatherService;
+
+import java.io.IOException;
+
 /*
  * ----------------------------------------------------------------------------
  * Copyright (c) 2019-2023 Marcus Medina, Campus Mölndal
@@ -7,20 +11,20 @@ package org.campusmolndal;
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  * ----------------------------------------------------------------------------
  */
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        String apiKey = "your_api_key_here";
+        WeatherService weatherService = new WeatherService(apiKey);
+        String city = "Gothenburg";
+        try {
+            String weatherData = weatherService.getWeatherInformation(city);
+            double temperature = weatherService.getTemperature(weatherData);
+            String description = weatherService.getWeatherDescription(weatherData);
+            System.out.println("The current temperature in " + city + " is " + temperature + "°C.");
+            System.out.println("The weather is described as: " + description + ".");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
---
+++ TDDproject/src/main/java/org/campusmolndal/Utmaning/WeatherService.java
@@ -0,0 +1,53 @@
+package org.campusmolndal.Utmaning;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+public class WeatherService {
+
+    private final String apiKey;
+
+    public WeatherService(String apiKey) {
+        this.apiKey = apiKey;
+    }
+
+    public String getWeatherInformation(String city) throws IOException {
+        // Replace with the actual API URL
+        String baseUrl = "https://api.example.com/weather";
+        String url = baseUrl + "?q=" + city + "&appid=" + apiKey;
+        URL apiUrl = new URL(url);
+        HttpURLConnection conn = (HttpURLConnection) apiUrl.openConnection();
+        conn.setRequestMethod("GET");
+
+        int responseCode = conn.getResponseCode();
+        if (responseCode == 200) {
+            Scanner scanner = new Scanner(conn.getInputStream());
+            StringBuilder response = new StringBuilder();
+            while (scanner.hasNextLine()) {
+                response.append(scanner.nextLine());
+            }
+            scanner.close();
+            return response.toString();
+        } else {
+            throw new IOException("Invalid city");
+        }
+    }
+
+    public double getTemperature(String city) {
+        // Parse the JSON response to extract the temperature
+        double temperature = Double.parseDouble(city.substring(city.indexOf("\"temp\":") + 7, city.indexOf(",\"feels_like\"")));
+        return temperature;
+    }
+
+    public String getWeatherDescription(String city) {
+        // Parse the JSON response to extract the weather description
+        String description = city.substring(city.indexOf("\"description\":\"") + 15, city.indexOf("\",\"icon\""));
+        return description;
+    }
+
+}
+
+
+
+

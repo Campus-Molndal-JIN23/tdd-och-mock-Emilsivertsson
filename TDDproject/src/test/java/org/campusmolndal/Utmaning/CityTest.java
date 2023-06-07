229d2084b5c45b542170fc8983f7646cedf695d4
Emilsivertsson <Emil.sivertsson@gmail.com>
2023-06-07 11:09:19 +0200

    reworked tests

--- TDDproject/src/test/java/org/campusmolndal/Utmaning/CityTest.java
+++ TDDproject/src/test/java/org/campusmolndal/Utmaning/CityTest.java
@@ -1,6 +1,5 @@
 package org.campusmolndal.Utmaning;
 
-import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
